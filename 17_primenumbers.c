28ea0bcdd79643a4607b84d1d325d350cb6a190b
Vyshalichitla <116860398+Vyshalichitla@users.noreply.github.com>
2023-07-11 21:14:55 +0530

    Add files via upload

---
+++ 17_primenumbers.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void generatePrimes(int start, int end) {
+    printf("Prime numbers between %d and %d are: ", start, end);
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int start, end;
+
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+
+    generatePrimes(start, end);
+
+    return 0;
+}
+
